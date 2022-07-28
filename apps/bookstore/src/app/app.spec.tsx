73ded6c7dcf83aba552842f09bc218dc8b1b9853
callmehandsomeboy <adhamkukuh@gmail.com>
2022-07-28 10:46:05 +0700

    first commit

--- apps/bookstore/src/app/app.spec.tsx
+++ apps/bookstore/src/app/app.spec.tsx
@@ -1,17 +1,13 @@
-import { render } from '@testing-library/react';
-
+import { render, cleanup } from '@testing-library/react';
 import App from './app';
-
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-
-    expect(getByText(/Welcome bookstore/gi)).toBeTruthy();
-  });
+afterEach(cleanup);
+it('should render successfully', () => {
+const { baseElement } = render(<App />);
+expect(baseElement).toBeTruthy();
+});
+it('should have a header as the title', () => {
+const { getByText } = render(<App />);
+expect(getByText('Bookstore')).toBeTruthy();
+});
 });
